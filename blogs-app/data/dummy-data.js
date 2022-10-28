c163d4f732048b9477ce25216f107978f45f0b1b
Saeed-Ameen <116690665+Saeed-Ameen@users.noreply.github.com>
2022-10-28 07:01:22 +0300

    Add files via upload

---
+++ blogs-app/data/dummy-data.js
@@ -0,0 +1,9 @@
+import Blog from '../models/blog';
+
+
+export const BLOGS = [
+  new Blog(1, 'OPEN Forum', 'American Express has really knocked it out of the park with OPEN Forum. They’re covering topics from agile marketing to building company culture and everything in between.'),
+  new Blog(2, 'Adrian Swinscoe', 'Blog of consultant and speaker Adrian Swinscoe, which features interviews with business people and posts about customer experience, building a customer-centric business, service, social media as well as employee and customer engagement.'),
+  new Blog (3 , 'Buffer' , 'All social everything! What I love the most about Buffer is their transparency. They are not afraid to share their data, what they’ve failed at, and what they’ve learned. They have a buzzing community of people who want to discuss what they’ve learned from the Buffer blog. Chat with Buffer’s content creators and grow in a fun, exciting community!'),
+
+];
